d43a4d0cf467a06eb2160809d3af1524aa1bf731
MarryBye <vlukianov04@gmail.coMarryBye>
2024-08-08 22:39:53 +0300

    11111

--- paths.py
+++ paths.py
@@ -1,5 +1,5 @@
 import os
 
-STATIC_PATH = os.path.join("mysite", "web")
+STATIC_PATH = os.path.join("LogikaShpora", "web")
 TEMPLATES_PATH = os.path.join(STATIC_PATH, "templates")
 LESSONS_PATH = os.path.join(TEMPLATES_PATH, "lessons")
